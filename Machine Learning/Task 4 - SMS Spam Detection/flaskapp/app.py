7e40736b60bcbbd1cc4e3965b04060b9a863cd5a
Kaus1kC0des <kausikdevanathan@gmail.com>
2024-01-16 16:41:06 +0530

    FINAL WORKING COMMIT
    Refactored the code according to PEP 8 and removed unnecessary files and functions

--- Machine Learning/Task 4 - SMS Spam Detection/flaskapp/app.py
+++ Machine Learning/Task 4 - SMS Spam Detection/flaskapp/app.py
@@ -1,19 +1,18 @@
+import pickle
+import string
+import nltk
+from flask import Flask, render_template, request, redirect, url_for
 from nltk.stem.porter import PorterStemmer
 from nltk.corpus import stopwords
-from flask import Flask, render_template, request
-import nltk
-import pandas as pd
-import numpy as np
 
 nltk.download("punkt")
 nltk.download("stopwords")
-import pickle
-
-import string
-from flask import redirect, url_for
 
 app = Flask(__name__, template_folder='templates')
 ps = PorterStemmer()
+prediction = ""
+model = pickle.load(open('models/final_model.pkl', 'rb'))
+vectorizer = pickle.load(open('models/vectorizer_final.pkl', 'rb'))
 
 
 def transform_text(text):
@@ -23,24 +22,10 @@ def transform_text(text):
     for i in text:
         if i.isalnum():
             y.append(i)
-    del text  # free up memory space
-    text = []
-    for i in y:
-        if i not in stopwords.words("english") and i not in string.punctuation:
-            text.append(i)
-    y.clear()
-    for i in text:
-        y.append(ps.stem(i))
+    text = [i for i in y if i not in stopwords.words("english") and i not in string.punctuation]
+    y = [ps.stem(i) for i in text]
     out = " ".join(y)
-    del y  # Free memory space
-    del text  # Free memory space
     return out
-
-
-prediction = ""
-
-model = pickle.load(open('models/final_model.pkl', 'rb'))
-vectorizer = pickle.load(open('models/vectorizer_final.pkl', 'rb'))
 
 
 @app.route("/")
@@ -58,22 +43,15 @@ def predict():
     if request.method == "POST":
         try:
             global prediction
-            message = ""
             message = request.form['emailContent']
-            # data = request.get_json()
-            # message = data.get('emailContent')
             query = transform_text(message)
             vect = vectorizer.transform([query]).toarray()
             out = model.predict(vect)
-            print(type(out[0]))
             if out == 0:
                 prediction = "HAM"
-                print(prediction)
                 return redirect(url_for('ham'))
-                # return output(prediction)
             else:
                 prediction = "SPAM!"
-                print(prediction)
                 return redirect(url_for("spam"))
         except Exception as e:
             return render_template("error.html", error_message=e)
@@ -81,13 +59,10 @@ def predict():
         return render_template("predict.html")
 
 
-@app.route("/output")
-def output(prediction):
-    return render_template('output.html', prediction=prediction)
-
 @app.route("/spam")
 def spam():
     return render_template("spam.html")
+
 
 @app.route("/ham")
 def ham():
